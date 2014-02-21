fc395bab36f9d2e3ff1e754ec5683d341cbfbd04
小泉智美 <kyon@dilly-dally.net>
2014-02-21 17:42:58 +0900

    gvimrcを含める

--- setup.sh
+++ setup.sh
@@ -17,3 +17,17 @@ do
     echo "シンボリックリンクを貼りました！: $file"
   fi
 done
+
+# gvimrc
+GVIMRC_FILE=( gvimrc )
+
+for vfile in ${GVIMRC_FILE[@]}
+do
+  if [ -a /Applications/MacVim.app/Contents/Resources/vim/$vfile ]; then
+    rm -f /Applications/MacVim.app/Contents/Resources/vim/$vfile
+    ln -s $HOME/dotfiles/$vfile /Applications/MacVim.app/Contents/Resources/vim/$vfile
+    echo "$vfile のシンボリックリンク貼ったよ"
+  else
+    echo "gvimrcがないでござる"
+  fi
+done
